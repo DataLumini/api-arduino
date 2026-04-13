7d44e10fc0e1d63de25bf75feff26d0e0eb2d2e2
gabrielGalhiardoFarina <gabriel.farina@sptech.school>
2026-04-13 14:46:38 -0300

    Adicionando o codigo do arduino para mostrar em PPFD

---
+++ codigo_arduido/arduido/arduido.ino
@@ -0,0 +1,25 @@
+const int PINO_SENSOR_LDR = A1;
+int valorLuminosidade;
+
+const float PPFD_fator = 0.017; 
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  valorLuminosidade = analogRead(PINO_SENSOR_LDR);
+
+  float voltagem = valorLuminosidade * (5.0 / 1023.0);
+  if (voltagem > 4.98) voltagem = 4.98;
+
+  float resistencia = (5.0 * 10000.0 / voltagem) - 10000.0;
+  float lux = 500.0 * pow((resistencia / 1000.0), -1.4); 
+
+  float ppfd = lux * PPFD_fator;
+
+  Serial.print("Luminosidade: ");
+  Serial.println(ppfd);
+  
+  delay(2000);
+}
